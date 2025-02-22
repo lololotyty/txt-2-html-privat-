f9e5ea57608da66d1d7ba6f8c0b0bdd84676a734
MARCO-015 <Charlie.aimers07@gmail.com>
2025-02-22 21:31:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import telebot
 import os
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-bot = telebot.TeleBot(BOT_TOKEN)
+API_ID = os.getenv("API_ID", "ac24e438ff9a0f600cf3283e6d60b1aa")
+API_HASH = os.getenv("API_HASH", "25579552")
+TOKEN = os.getenv("BOT_TOKEN", "7548242755:AAGiLXS6Qc2ZCPksD73t7yVlPKeFi4w86gM")
+
+
+bot = telebot.TeleBot(TOKEN)
 
 # Function to convert TXT to HTML
 def txt_to_html(txt_path, html_path):
